2be62f335099af4d912cb161cb362501221c64d0
fcocervera <fcocervera@users.noreply.github.com>
2016-08-12 19:02:33 -0700

    Update application.py

--- application.py
+++ application.py
@@ -33,25 +33,25 @@ def predict():
     print recs
 
     return render_template('response.html', 
-    						rec_one=recs['title'][0],
-    						rec_two=recs['title'][1],
-    						rec_three=recs['title'][2],
-    						rec_four=recs['title'][3],
-    						rec_five=recs['title'][4],
+    			rec_one=recs['title'][0],
+    			rec_two=recs['title'][1],
+    			rec_three=recs['title'][2],
+    			rec_four=recs['title'][3],
+    			rec_five=recs['title'][4],
 
-                            pic_one=imgUrls[recs['title'][0]][0],
-                            pic_two=imgUrls[recs['title'][1]][0],
-                            pic_three=imgUrls[recs['title'][2]][0],
-                            pic_four=imgUrls[recs['title'][3]][0],
-                            pic_five=imgUrls[recs['title'][4]][0], 
-
-                            rating_one=imgUrls[recs['title'][0]][1],
-                            rating_two=imgUrls[recs['title'][1]][1],
-                            rating_three=imgUrls[recs['title'][2]][1],
-                            rating_four=imgUrls[recs['title'][3]][1],
-                            rating_five=imgUrls[recs['title'][4]][1]
-
-                            )
+                        pic_one=imgUrls[recs['title'][0]][0],
+	                pic_two=imgUrls[recs['title'][1]][0],
+	                pic_three=imgUrls[recs['title'][2]][0],
+	                pic_four=imgUrls[recs['title'][3]][0],
+	                pic_five=imgUrls[recs['title'][4]][0], 
+	
+	                rating_one=imgUrls[recs['title'][0]][1],
+	                rating_two=imgUrls[recs['title'][1]][1],
+	                rating_three=imgUrls[recs['title'][2]][1],
+	                rating_four=imgUrls[recs['title'][3]][1],
+	                rating_five=imgUrls[recs['title'][4]][1]
+	
+	                )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8090, debug=True)
